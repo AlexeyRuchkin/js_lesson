7c974bb042803c867f0f0165a775eb79ee7c9055
Alex <mazay_ne@bk.ru>
2018-08-10 02:48:21 +0600

    Second commit 2,0

--- lesson2/src/js/script.js
+++ lesson2/src/js/script.js
@@ -9,20 +9,48 @@ let mainList = {
 		shopGoods: [],
 		employers: {},
 		open: false,
-		 
 }
 
-while (num < 5) {
+
+
+/*while (num < 5)  {
 	let a = prompt ("Какой тип товаров будем продавать?");
 	num++;
-	// statement
+	
 
 	if ((typeof(a)) === 'string' && (typeof(a)) === null && a != '' && a.length < 50) {
 		console.log('Все верно!');
 		mainList.shopGoods[i] = a;
 	}
+	
+}
+*/
+
+for (let i = 0; i < 5; i++) {
+
+	let a = prompt ("Какой тип товаров будем продавать?");
+	if ((typeof(a)) === 'string'  && (typeof(a)) === null && a != '' && a.length < 50) {
+		console.log('Все верно!');
+		mainList.shopGoods[i] = a;
+	} else {
+		a = prompt ("Какой тип товаров будем продавать?");
+
+	}
+	
 }
 
+
+/*do {
+	let a = prompt ("Какой тип товаров будем продавать?");
+	num++;
+	
+
+	if ((typeof(a)) === 'string' && (typeof(a)) === null && a != '' && a.length < 50) {
+		console.log('Все верно!');
+		mainList.shopGoods[i] = a;
+	}	
+}
+while (num < 5);*/
 
 alert(mainList.budget / 30);
 
